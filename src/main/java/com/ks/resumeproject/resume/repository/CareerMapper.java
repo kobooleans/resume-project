2bc18e9dd11f02b5ec842a68381ccb24bbbd74e1
조현준 <45157196+koboolean@users.noreply.github.com>
2024-08-16 22:44:32 +0900

    커리어 부분 수정 및 캘린더 관련사항 3차 반영

---
+++ src/main/java/com/ks/resumeproject/resume/repository/CareerMapper.java
@@ -0,0 +1,14 @@
+package com.ks.resumeproject.resume.repository;
+
+import com.ks.resumeproject.resume.domain.CareerDto;
+import com.ks.resumeproject.security.domain.AccountDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CareerMapper {
+    List<CareerDto> getCareerList(AccountDto accountDto);
+
+    void insertCareer(CareerDto careerDto);
+}
