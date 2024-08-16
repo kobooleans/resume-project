64998c2db1ed6e840da15029c56ee219aeb21d35
조현준 <45157196+koboolean@users.noreply.github.com>
2024-08-16 22:44:32 +0900

    커리어 부분 수정 및 캘린더 관련사항 3차 반영

---
+++ src/main/java/com/ks/resumeproject/resume/domain/CareerDto.java
@@ -0,0 +1,23 @@
+package com.ks.resumeproject.resume.domain;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+@Data
+public class CareerDto {
+    private String username;
+    private BigInteger careerId;
+    private BigInteger id;
+    private String randomId;
+    private Date startDay;
+    private Date endDay;
+    private String companyName;
+    private String role;
+    private String position;
+    private String job;
+    private String salary;
+    private String location;
+    private String content;
+}
