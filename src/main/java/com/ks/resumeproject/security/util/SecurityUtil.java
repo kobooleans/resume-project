c38c0ac8d14b48a689935b9ecc51057724f9b07c
조현준 <45157196+koboolean@users.noreply.github.com>
2024-08-19 23:38:39 +0900

    SecurityUtil 수정반영

--- src/main/java/com/ks/resumeproject/security/util/SecurityUtil.java
+++ src/main/java/com/ks/resumeproject/security/util/SecurityUtil.java
@@ -4,6 +4,7 @@ import com.ks.resumeproject.security.domain.AccountContext;
 import com.ks.resumeproject.security.domain.AccountDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
@@ -13,10 +14,11 @@ public class SecurityUtil {
     private static Logger logger = LoggerFactory.getLogger(SecurityUtil.class);
 
     public static AccountContext getAccount() {
-        if(!(SecurityContextHolder.getContext().getAuthentication().isAuthenticated())){
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if(!(authentication.isAuthenticated())){
             logger.error("로그인하지 않은 사용자는 해당 값을 가지고올 수 없습니다. 확인 바랍니다.");
             return null;
         }
-        return ((AccountContext) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return ((AccountContext) authentication.getPrincipal());
     }
 }
