3f492aebc40b4724fa7363952c7c1431136d2228
조현준 <45157196+koboolean@users.noreply.github.com>
2024-08-21 14:59:09 +0900

    ComUtil으로 분리 및 Component 부분 추가, Map putAll 관련기능 추가 반영

--- src/main/java/com/ks/resumeproject/util/WebUtil.java
+++ src/main/java/com/ks/resumeproject/util/WebUtil.java
@@ -19,16 +19,5 @@ public class WebUtil {
         return request.getHeader(CONTENT_TYPE).contains(CONTENT_TYPE_JSON);
     }
 
-    public static String addRandomVal() {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
 
-        return random.ints(leftLimit,rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-    }
 }
