1948b01783c34e2e3d5a98127175e9c15a185ea4
eo339912 <eo339912@gmail.com>
2024-08-28 21:40:35 +0900

    이력서 - 자기소개서 기능 추가

--- src/main/java/com/ks/resumeproject/resume/repository/CoverLetterMapper.java
+++ src/main/java/com/ks/resumeproject/resume/repository/CoverLetterMapper.java
@@ -1,16 +1,10 @@
 package com.ks.resumeproject.resume.repository;
 
 import com.ks.resumeproject.resume.domain.CoverLetterDto;
-import com.ks.resumeproject.resume.service.CoverLetterService;
 import com.ks.resumeproject.security.domain.AccountDto;
-import com.ks.resumeproject.security.util.SecurityUtil;
-import com.ks.resumeproject.users.service.SecurityService;
-import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface CoverLetterMapper {
@@ -22,5 +16,5 @@ public interface CoverLetterMapper {
 
     int deleteCoverLetter(CoverLetterDto coverLetterDto);
 
-    int updateCoverLetterList(CoverLetterDto coverLetterDto);
+    void updateCoverLetterList(CoverLetterDto coverLetterDto);
 }
