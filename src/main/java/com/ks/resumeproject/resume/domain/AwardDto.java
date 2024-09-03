ceac02a2aa4a1b7a5c8dce5e5eba9c42b0e9ae8e
eo339912 <eo339912@gmail.com>
2024-09-03 21:48:32 +0900

    이력서 - 자격/수상/어학 기능 추가

---
+++ src/main/java/com/ks/resumeproject/resume/domain/AwardDto.java
@@ -0,0 +1,34 @@
+package com.ks.resumeproject.resume.domain;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Data
+public class AwardDto {
+    private String awardKey;
+    private BigInteger id;
+    private String randomId;
+    private BigInteger seq;
+
+    //자격
+    private BigInteger licenseId;
+    private String licenseNm;
+    private String licenseIssueOrg;
+    private String licensePassYn;
+    private String licensePassDate;
+
+    //어학
+    private BigInteger langTestId;
+    private String langNm;
+    private String langTestNm;
+    private String langAcquireDate;
+    private String langTestScore;
+    private String langAcquireYn;
+
+    //수상
+    private BigInteger awardId;
+    private String awardNm;
+    private String awardDate;
+    private String awardOrg;
+}
