75cefa69d72bd4024f1643aaa6571c6f47b50b2c
조현준 <45157196+koboolean@users.noreply.github.com>
2024-11-14 22:54:40 +0900

    ResponseEntity 관련사항 반영

--- src/main/java/com/ks/resumeproject/resume/service/impl/MyInfoServiceImpl.java
+++ src/main/java/com/ks/resumeproject/resume/service/impl/MyInfoServiceImpl.java
@@ -3,6 +3,7 @@ package com.ks.resumeproject.resume.service.impl;
 import com.ks.resumeproject.resume.domain.MyinfoDto;
 import com.ks.resumeproject.resume.domain.ResumeDto;
 import com.ks.resumeproject.resume.repository.MyInfoMapper;
+import com.ks.resumeproject.resume.repository.ResumeMapper;
 import com.ks.resumeproject.resume.service.MyInfoService;
 import com.ks.resumeproject.security.domain.AccountDto;
 import com.ks.resumeproject.users.service.SecurityService;
@@ -15,11 +16,32 @@ public class MyInfoServiceImpl implements MyInfoService {
 
     private final MyInfoMapper myInfoMapper;
     private final SecurityService securityService;
+    private final ResumeMapper resumeMapper;
 
     @Override
     public MyinfoDto getMyInfo(AccountDto accountDto) {
         AccountDto account = securityService.selectUserAccount(accountDto.getUsername());
         accountDto.setId(account.getId());
+
+        ResumeDto resumeDto = resumeMapper.getResumeSet(accountDto);
+
+
+        /** 만약 처음 resume으로 접근했을 경우 모두 false로 생성 */
+        if(resumeDto == null){
+
+            resumeDto = new ResumeDto();
+            resumeDto.setId(accountDto.getId());
+            resumeDto.setRandomId(accountDto.getRandomId());
+            resumeDto.setSkillYn(false);
+            resumeDto.setCareerYn(false);
+            resumeDto.setEduYn(false);
+            resumeDto.setAwardYn(false);
+            resumeDto.setActionYn(false);
+            resumeDto.setCoverLetterYn(false);
+
+            resumeMapper.setResumeSet(resumeDto);
+
+        }
 
         MyinfoDto myinfoDto = myInfoMapper.getMyInfo(accountDto);
 
