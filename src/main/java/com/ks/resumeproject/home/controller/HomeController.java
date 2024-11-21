9e763c2c8ea12a268d333c45a591fed322952447
조현준 <45157196+koboolean@users.noreply.github.com>
2024-11-21 23:25:46 +0900

    icon 관련사항 commit

--- src/main/java/com/ks/resumeproject/home/controller/HomeController.java
+++ src/main/java/com/ks/resumeproject/home/controller/HomeController.java
@@ -2,16 +2,20 @@ package com.ks.resumeproject.home.controller;
 
 import com.ks.resumeproject.home.service.HomeService;
 import com.ks.resumeproject.home.domain.TitleDto;
+import com.ks.resumeproject.manage.domain.SiteInfoDto;
 import com.ks.resumeproject.security.domain.AccountDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,8 +28,16 @@ public class HomeController {
 
     @Operation(summary = "타이틀 조회", description = "타이틀 정보를 가지고 온다.")
     @PostMapping("/selectTitle")
-    public TitleDto selectTitle(@RequestBody AccountDto accountDto){
-        return homeService.selectTitle(accountDto);
+    public ResponseEntity<Map> selectTitle(@RequestBody AccountDto accountDto){
+        Map map = new HashMap();
+
+        TitleDto titleDto = homeService.selectTitle(accountDto);
+        map.put("title",titleDto);
+
+        List<SiteInfoDto> info = homeService.selectSiteInfo(accountDto);
+        map.put("siteInfo",info);
+
+        return ResponseEntity.ok(Map.of("result",map));
     }
 
     @Operation(summary = "타이틀 수정", description = "타이틀 정보를 수정한다.")
---
+++ src/main/java/com/ks/resumeproject/manage/controller/SiteInfoController.java
@@ -0,0 +1,55 @@
+package com.ks.resumeproject.manage.controller;
+
+import com.ks.resumeproject.manage.domain.SiteInfoDto;
+import com.ks.resumeproject.manage.service.SiteInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/siteInfo")
+@RequiredArgsConstructor
+@Tag(name = "SiteInfo", description = "하단 사이트 관리")
+public class SiteInfoController {
+
+    private final SiteInfoService siteInfoService;
+
+
+    @Operation(summary = "하단 사이트 조회", description = "하단 사이트를 조회한다.")
+    @GetMapping("/select")
+    public ResponseEntity<Map<String, List<SiteInfoDto>>> select(){
+        List<SiteInfoDto> siteInfoDtos = siteInfoService.getFooterSiteList();
+
+        return ResponseEntity.ok(Map.of("result", siteInfoDtos));
+    }
+
+
+    @Operation(summary = "하단 사이트 등록", description = "하단 사이트를 등록한다.")
+    @PostMapping("/insert")
+    public ResponseEntity<Map> insert(@RequestBody SiteInfoDto siteInfoDto){
+        siteInfoService.insertFooterSite(siteInfoDto);
+
+        return ResponseEntity.ok(Map.of("result","success"));
+    }
+
+    @Operation(summary = "하단 사이트 삭제", description = "하단 사이트를 삭제한다.")
+    @PostMapping("/delete")
+    public ResponseEntity<Map> delete(@RequestBody List<SiteInfoDto> siteInfoDto){
+        siteInfoService.deleteFooterSite(siteInfoDto);
+
+        return ResponseEntity.ok(Map.of("result","success"));
+    }
+
+    @Operation(summary = "하단 사이트 수정", description = "하단 사이트를 수정한다.")
+    @PostMapping("/update")
+    public ResponseEntity<Map> update(@RequestBody SiteInfoDto siteInfoDto){
+        siteInfoService.updateFooterSite(siteInfoDto);
+
+        return ResponseEntity.ok(Map.of("result","success"));
+    }
+}
