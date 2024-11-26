ed3d308fc25e966f248c1a12f882781f26bbc337
조현준 <45157196+koboolean@users.noreply.github.com>
2024-11-26 14:12:46 +0900

    권한 관련 취약점 조치반영

--- src/main/java/com/ks/resumeproject/error/domain/ErrorCode.java
+++ src/main/java/com/ks/resumeproject/error/domain/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     UNKNOWN_EXCEPTION(HttpStatus.BAD_REQUEST, "UNKNOWN-EXCEPTION", "알수없는 오류가 발생하였습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH-F01", "로그인되지 않은 사용자이므로, 접근을 할 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH-F01", "정보가 만료되어 로그아웃 되었습니다. 로그인되지 않은 사용자이므로, 접근을 할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
